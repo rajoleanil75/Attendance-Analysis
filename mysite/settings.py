00bb409cbb9bac04f6ec57891483491969411cf6
anil <rajoleanil75@gmail.com>
2017-08-30 18:38:33 +0530

    Mysql db file username updatd

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test',
-        'USER': 'anilrajole',
+      #  'NAME': 'test',
+        'USER': 'anilrajole$test',
         'PASSWORD': 'test1234',
         'HOST': 'anilrajole.mysql.pythonanywhere-services.com',
         'PORT': '3306',
