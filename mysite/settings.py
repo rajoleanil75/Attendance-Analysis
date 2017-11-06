617b5dedff7ab16e121e1b477bb6fd81b826f411
anil <rajoleanil75@gmail.com>
2017-11-06 07:07:54 +0530

    Settings.py change

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,12 +29,12 @@ DEBUG = True
 ALLOWED_HOSTS = ['anilraj.pythonanywhere.com']
 
 # Application definition
-#EMAIL_BACKEND = ''
-#EMAIL_HOST = ""
-#EMAIL_HOST_USER = ""
-#EMAIL_HOST_PASSWORD = ''
-#EMAIL_PORT = 587
-#EMAIL_USE_TLS = True
+EMAIL_BACKEND = ''
+EMAIL_HOST = ""
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 INSTALLED_APPS = [
 	'graphos',
